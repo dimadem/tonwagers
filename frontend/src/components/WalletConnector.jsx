bb97c0b36188b3e4a76a94bca51f60e5194b72df
Detectivevincent <knight.fo@yandex.ru>
2023-02-26 11:15:28 +0000

    wallCon

--- frontend/src/components/WalletConnector.jsx
+++ frontend/src/components/WalletConnector.jsx
@@ -1,8 +1,13 @@
-export default function WalletConnector(params) {
-  return (
-    <div>
-      <h1>Wallet Connector</h1>
-      <button onClick={connectWallet}>Connect Wallet</button>
-    </div>
-  );
-}
+import { useTonAddress } from '@tonconnect/ui-react';
+export const Wallet = () => {
+    const wallet = useTonWallet();
+
+    return (
+        wallet && (
+            <div>
+                <span>Connected wallet: {wallet.name}</span>
+                <span>Device: {wallet.device.appName}</span>
+            </div>
+        )
+    );
+};
